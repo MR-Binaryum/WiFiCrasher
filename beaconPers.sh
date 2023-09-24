45289712a51f5b5cf6bcdffa48e408fe07772216
MR-Binaryum <133718570+MR-Binaryum@users.noreply.github.com>
2023-09-24 10:26:13 +0000

    Add files via upload

---
+++ beaconPers.sh
@@ -0,0 +1,12 @@
+ echo ""
+        echo $purple "╭─"$green"Put the name for fake AP's (-_-) "$purple
+                           read -p " ╰─$ " get
+
+
+for i in {1..100}; do 
+
+
+echo $get$i >> names
+
+done
+echo ""

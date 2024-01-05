5b0fd0f00934639a894f0b956abbc25f250bdbc5
MR-Binaryum <133718570+MR-Binaryum@users.noreply.github.com>
2024-01-05 07:32:45 +0000

    Add files via upload

---
+++ VIRTUALinterfaces_conf.sh
@@ -0,0 +1,71 @@
+
+crafter(){
+for i in {1..100}; do
+    sudo iw dev wlo1mon interface add "mon$i" type monitor
+    if [ $? -eq 0 ]; then
+        echo "Interface mon$i crafted..."
+    else
+        echo "Error to craft interface mon$i."
+    fi
+done
+}
+
+deleter(){
+for i in {1..100}; do
+    sudo iw dev "mon$i" del
+    if [ $? -eq 0 ]; then
+        echo "Interface mon$i deleted..."
+    else
+        echo "Error to make mon$i..."
+    fi
+done
+}
+
+
+spooferVirt(){
+
+file="FAKEMAList.txt"
+
+mapfile -t mac_array < "$file"
+
+numINT=100
+
+for ((i=1; i<=$numINT; i++)); do
+
+random=$((RANDOM % ${#mac_array[@]}))
+
+sudo ip link set dev "mon$i" down
+
+
+sudo ip link set dev "mon$i" address "${mac_array[$random]}"
+sudo ip link set dev "mon$i" up
+
+unset mac_array[$random] 
+mac_array=("${mac_array[@]}")
+
+done
+
+}
+
+echo    $purple"{"$cyan"1"$purple"}"$yellow"--"$green"Craft 15 virtual interfaces in monitor mode to attack multiple networks at the same time"
+    echo    $purple"{"$cyan"2"$purple"}"$yellow"--"$green"Delete 15 virtual interfaces in monitor mode to attack multiple networks"
+    echo    $purple"{"$cyan"3"$purple"}"$yellow"--"$green"Spoof MAC address from mon1..100 virtual interfaces"
+    echo
+        echo $purple "╭─"$green"Select configure mode (-_-) "$purple
+            read -p " ╰─$ " selection
+
+            if [ $selection -eq 1 ];
+            then
+               crafter
+
+            elif [ $selection -eq 2 ];
+            then
+                deleter
+
+            elif [ $selection -eq 3 ];
+            then
+                spooferVirt
+
+else echo "brooo look this dude XD..."
+
+fi

